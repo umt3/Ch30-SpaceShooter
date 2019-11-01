11019657183d7161567e1a5674054e32131b2133
Urvee Trivedi <urvee.trivedi@rutgers.edu>
2019-11-01 16:20:19 -0400

    complete code

---
+++ Assets/Enemy.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("set in Inspector: Enemy")]
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100;
+
+    private BoundsCheck bndCheck;
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    // this is a property: a method that acts like a field
+
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+ void Update()
+    {
+        Move();  
+   
+    if(bndCheck != null && !bndCheck.isOnScreen)
+        {
+            Destroy(gameObject);
+            //check to make sure its gone off the bottom of the screen
+            if (pos.y < bndCheck.camHeight - bndCheck.radius)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+
+
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Shield.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+    public float rotationsPerSecond = 0.1f;
+
+    [Header("Set Dynamically")]
+    public int levelShown = 0;
+
+    //this non-piblic varibale will not appear in the inspector
+    Material    mat;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //read the current shield level from the Hero Singleton
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+        //if this is different from level showb...
+        if(levelShown != currLevel)
+        {
+            levelShown = currLevel;
+            //Adjust the texture offset to show different shield level
+            mat.mainTextureOffset = new Vector2(0.2f * levelShown, 0);
+                   
+         }
+        //rotate the shield a bit every framw in a time-based way
+        float rZ = -(rotationsPerSecond * Time.time * 360) % 360f;
+        transform.rotation = Quaternion.Euler(0, 0, rZ);
+    }
+}
