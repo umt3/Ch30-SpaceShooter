5ce979b506edece0945870083c5a362802f0563e
Urvee Trivedi <urvee.trivedi@rutgers.edu>
2019-11-01 21:41:47 -0400

    Works!

---
+++ Assets/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private BoundsCheck bndCheck;
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+
+
+    }
+
+    void Update()
+    {
+        if (bndCheck.offUp)
+            Destroy(gameObject);
+    }
+}
+
+
+
